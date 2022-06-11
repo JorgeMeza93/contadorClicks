7ed29c2aa0f1ba76689b03da38f3bb76da1d4828
Hayda Bello <haydabello@iMac-de-Hayda.local>
2022-06-11 14:33:12 -0500

    Componente boton realizado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import freeCodeCampLogo from "./imagenes/freeCodeCampLogo.svg"
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="freecodecamp-logo-contenedor">
+        <img src={freeCodeCampLogo} alt="logo de freecodecamp" className="freecodecamp-logo" />
+      </div>
+      <div className="contenedor-principal">
+        
+      </div>
     </div>
   );
 }
