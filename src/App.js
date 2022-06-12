aab221536224edbf7f8ddff2dd6be5c30c2f9bb1
Hayda Bello <haydabello@iMac-de-Hayda.local>
2022-06-12 01:08:44 -0500

    Contador completado

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import freeCodeCampLogo from "./imagenes/freeCodeCampLogo.svg"
 import Boton from './componentes/Boton';
-
+import Contador from './componentes/Contador';
+import { useState } from "react";
 function App() {
   
+  const [numClicks, setClicks] = useState(0);
+
   const Clickboton = () => {
-    console.log("Hola mundo");
+    setClicks(numClicks +1) ;
   }
   const reiniciarContador = () => {
-    console.log("Reiniciando contador")
+    setClicks((0));
+    numClicks = 0;
   }
 
   return (
@@ -17,6 +21,7 @@ function App() {
         <img src={freeCodeCampLogo} alt="logo de freecodecamp" className="freecodecamp-logo" />
       </div>
       <div className="contenedor-principal">
+        <Contador clicks={ numClicks } />
         <Boton 
           texto="Click"
           botonDeClick={true}
--- src/componentes/Contador.jsx
+++ src/componentes/Contador.jsx
@@ -1,1 +1,12 @@
 import React from "react";
+import "../styles/Contador.css";
+
+function Contador( { clicks } ){
+    return (
+        <div className="contador">
+            {clicks}
+        </div>
+    );
+}
+
+export default Contador;
